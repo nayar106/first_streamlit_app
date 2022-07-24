e6e87c6dd581b9803be35ce3fb837d49ec7710c4
ABHISHEK KUMAR <abhishek.kum106@gmail.com>
2022-07-24 16:59:32 +0530

    Update streamlit_app.py
    added new breakfast menu with logo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title('my parent new healthy dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Bluebarry oatmeal')
-streamlit.text('kale,spinch & Rocket smoothie')
-streamlit.text('Hard boiled free range agg ')
+streamlit.text('🥣 Omega 3 & Bluebarry oatmeal')
+streamlit.text('🥗kale,spinch & Rocket smoothie')
+streamlit.text(' 🐔 Hard boiled free range agg ')
+streamlit.text('🥑🍞 Avocado,Toast ')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
