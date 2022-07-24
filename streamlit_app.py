5afe99a8a0a7557567edc1fc54a046e371baf818
ABHISHEK KUMAR <abhishek.kum106@gmail.com>
2022-07-24 16:54:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('my parent new healthy dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Bluebarry oatmeal')
+streamlit.text('kale,spinch & Rocket smoothie')
+streamlit.text('Hard boiled free range agg ')
