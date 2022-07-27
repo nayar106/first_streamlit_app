8688c1bac999c32ee549403dbf80a77b186cf33e
ABHISHEK KUMAR <abhishek.kum106@gmail.com>
2022-07-27 11:50:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,4 +24,4 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.dataframe(fruityvice_response.json())
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#streamlit.dataframe(fruityvice_normalized)
+streamlit.dataframe(fruityvice_normalized)
